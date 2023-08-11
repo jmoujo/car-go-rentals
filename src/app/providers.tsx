8d798113b846f5e22067cd2c97a355eece10e34f
ankomahene <ankomaheneshadrack@gmail.com>
2023-08-11 13:02:19 +0000

    initial setup with mantine ui working with next.js app dir

---
+++ src/app/providers.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  useEmotionCache,
+} from '@mantine/core';
+import { useColorScheme, useHotkeys, useLocalStorage } from '@mantine/hooks';
+import { useServerInsertedHTML } from 'next/navigation';
+
+export default function RootStyleRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'carGo-theme',
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true,
+  });
+
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(' '),
+      }}
+    />
+  ));
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
+  return (
+    <CacheProvider value={cache}>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{
+            colorScheme,
+          }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          {children}
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </CacheProvider>
+  );
+}
---
+++ src/components/ButtonTest.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { Button } from '@mantine/core';
+import React from 'react';
+
+export const ButtonTest = () => {
+  return <Button m={6}>Click Me</Button>;
+};
